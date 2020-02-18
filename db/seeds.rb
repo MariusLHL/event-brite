e38d8cd127db32fad07a61bb022384a35aeb4323
mariuslhl <mariuslhl@gmail.com>
2020-02-18 23:39:48 +0100

    gogo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,17 @@
 #<User id: nil, first_name: nil, last_name: nil, description: nil, email: nil, password_digest: nil, age: nil, created_at: nil, updated_at: nil>
 #<JoinEvent id: nil, user_id: nil, event_id: nil, created_at: nil, updated_at: nil>
 User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
+5.times do
+ user = User.create(first_name:Faker::Name.first_name  , last_name: Faker::Name.last_name , description: Faker::Quote.famous_last_words , email: Faker::Internet.email, age: rand(18..78), password: "password")
+end
 
 10.times do
- user = User.create(first_name:Faker::Name.first_name  , last_name: Faker::Name.last_name , description: Faker::Quote.famous_last_words , email: Faker::Internet.email, age: rand(18..78), password: "password")
-  user.errors
+  vent = Event.create(title:  Faker::Quote.famous_last_words ,location: Faker::Address.full_address , description: Faker::Quote.famous_last_words , duration: 70, start: Time.now + 70, price: rand(1..100),user_id: rand(User.first.id..User.last.id))
+end
+
+100.times do
+  Attendance.create(user_id: rand(User.first.id..User.last.id), event_id:rand(Event.first.id..Event.last.id))
 end
