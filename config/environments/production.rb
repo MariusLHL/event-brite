f1d56e09a74c88d40b4f1a0f521a5a5181f5d2b8
mariuslhl <mariuslhl@gmail.com>
2020-02-18 12:34:04 +0100

    gogo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'lhl-event-brite.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'lhl-event-brite.herokuapp.com' }
 end
